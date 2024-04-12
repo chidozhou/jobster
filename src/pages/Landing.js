3ad47f7b0f4d1f097fa9b5ca88ae3124d2006dff
chidozhou <chidochashegerald@gmail.com>
2024-04-12 13:28:05 +0300

    first commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,31 @@
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <h5>
+            Take the reins of your career journey – track, apply, succeed with
+            ease. Your dream job awaits!
+          </h5>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
